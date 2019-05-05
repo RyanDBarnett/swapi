2491bd469916579fa5d16a1cb26a832e2ef94c1e
RyanDBarnett <conner2468@gmail.com>
2019-05-05 16:22:45 -0600

    Add Card snapshot test

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Card from './Card.js';
 import { shallow } from 'enzyme';
 
+const mockPerson = {
+  name: 'Luke Skywalker',
+  species: 'Human',
+  homeworld: 'Tatooine',
+  population: '200000'
+}
+const mockPlanet = {
+
+}
+const mockVehicle = {
+
+}
+
 const mockAddFavorite = jest.fn()
 const mockRemoveFavorite = jest.fn()
-const mockPerson = {
-  name: 'Luke',
-  species: 'Human',
-  homeworld: 'Tattoo',
-  population: '200'
-}
 
 describe('Card', () => {
   let wrapper = shallow(
