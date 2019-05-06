3da3fb04d99c0415a0d087ac71a08f3f501d8407
RyanDBarnett <conner2468@gmail.com>
2019-05-06 00:03:26 -0600

    Create buttons tests

---
+++ src/components/Buttons/Buttons.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Buttons from './Buttons.js';
+import {shallow} from 'enzyme';
+
+const mockTotalFavorites = 0;
+const mockChangeCategory = jest.fn();
+
+describe('App', () => {
+  let wrapper;
+
+  beforeEach( () => {
+    wrapper = shallow(
+      <Buttons 
+        totalFavorites={mockTotalFavorites} 
+        changeCategory={mockChangeCategory}
+      />
+    );
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should invoke changeCategory when a button is clicked', () => {
+    wrapper.find('button').first().simulate('click');
+    expect(mockChangeCategory).toBeCalled();
+  });
+});
