ba12c9484547aee0489df9e3d0f28742194c6820
RyanDBarnett <conner2468@gmail.com>
2019-05-06 09:42:43 -0600

    Updates

---
+++ precache-manifest.8b913d9bd8df5b75bf808843df29fbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4c98ab79e99dd506ac9d3475323fe5",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "dfa71dff478d4f490fe6",
+    "url": "/swapi/static/css/main.8101dd77.chunk.css"
+  },
+  {
+    "revision": "242a63258af3b4652269",
+    "url": "/swapi/static/js/2.119d4308.chunk.js"
+  },
+  {
+    "revision": "dfa71dff478d4f490fe6",
+    "url": "/swapi/static/js/main.f2b0deb9.chunk.js"
+  },
+  {
+    "revision": "86816fcca6b92ac284f8",
+    "url": "/swapi/static/js/runtime~main.3fb9cbf6.js"
+  },
+  {
+    "revision": "0a68e6599f5504ded0634868790d90a4",
+    "url": "/swapi/static/media/star-background.0a68e659.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9affab468ba3c2aea0a7a5fe2154a62d.js"
+  "/swapi/precache-manifest.8b913d9bd8df5b75bf808843df29fbff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swapi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
