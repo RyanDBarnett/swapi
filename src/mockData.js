0cd4515cc682307c22f6150d49c386d078111c53
RyanDBarnett <conner2468@gmail.com>
2019-05-05 23:22:46 -0600

    Create mockData

---
+++ src/mockData.js
@@ -0,0 +1,42 @@
+export const mockPerson = {
+  Homeworld: "Tatooine",
+  Name: "Luke Skywalker",
+  Population: "200000",
+  Species: "Human",
+  id: 1557118669139
+}
+
+const mockPerson2 = {
+  Homeworld: "Tatooine",
+  Name: "C-3PO",
+  Population: "200000",
+  Species: "Droid",
+  id: 1557118669141
+}
+
+export const mockPlanet = {
+  Climate: "temperate",
+  Name: "Alderaan",
+  Population: "2000000000",
+  Residents: 3,
+  Terrain: "grasslands, mountains",
+  id: 1557118667153
+}
+
+export const mockVehicle = {
+  Class: "wheeled",
+  Model: "Digger Crawler",
+  Name: "Sand Crawler",
+  Passenger Capacity: "30",
+  id: 1557118671153
+}
+
+export const mockPeople = [
+  mockPerson, mockPerson2
+]
+
+export const mockFavorites = [
+  mockPerson, 
+  mockVehicle,
+  mockPlanet
+]
